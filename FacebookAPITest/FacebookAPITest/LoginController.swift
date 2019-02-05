dda17dc91d3f1c1b8f8cedcebf7c2b796b3d9504
Suresh N <forsuresh@yandex.com>
2019-02-05 10:17:54 -0500

    Project starts without Storyboard

---
+++ FacebookAPITest/FacebookAPITest/LoginController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FacebookAPITest
+//
+//  Created by admin on 2/5/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        view.backgroundColor = UIColor.lightGray
+        
+        
+        
+    }
+
+
+}
+
